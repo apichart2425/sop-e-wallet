88a764246027683b99a085af7904a7cf04e0ed6a
apichart2425 <packapichart@gmail.com>
2019-10-14 14:59:54 +0700

    add field mode log

--- transactions/src/main/java/sop/service/transactions/model/Log.java
+++ transactions/src/main/java/sop/service/transactions/model/Log.java
@@ -15,9 +15,83 @@ public class Log {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-//    var title: String? = null,
-//    @OneToMany(mappedBy = "todo", fetch = FetchType.EAGER, cascade = arrayOf(CascadeType.REMOVE), orphanRemoval = true)
-//    var todoItems: List<TodoItem>? = emptyList()) {
-//    override fun toString(): String {
-//        return "Todo(id=$id, title=$title, todoItems=$todoItems)"
+    @NotNull(message = "account_source is required")
+    public int account_source;
+
+    @NotNull(message = "account_destination is required")
+    public int account_destination;
+
+    @NotNull(message = "balance is required")
+    @Min(0)
+    public double balance;
+
+    @NotNull(message = "balance is required")
+    public String service;
+
+
+    @NotNull(message = "balance is required")
+    public String currency_source;
+
+    @NotNull(message = "balance is required")
+    public String currency_destination;
+
+    @NotNull(message = "balance is required")
+    public Date created_on;
+
+    public int getAccount_source() {
+        return account_source;
+    }
+
+    public void setAccount_source(int account_source) {
+        this.account_source = account_source;
+    }
+
+    public int getAccount_destination() {
+        return account_destination;
+    }
+
+    public void setAccount_destination(int account_destination) {
+        this.account_destination = account_destination;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getCurrency_source() {
+        return currency_source;
+    }
+
+    public void setCurrency_source(String currency_source) {
+        this.currency_source = currency_source;
+    }
+
+    public String getCurrency_destination() {
+        return currency_destination;
+    }
+
+    public void setCurrency_destination(String currency_destination) {
+        this.currency_destination = currency_destination;
+    }
+
+    public Date getCreated_on() {
+        return created_on;
+    }
+
+    public void setCreated_on(Date created_on) {
+        this.created_on = created_on;
+    }
 }
+
