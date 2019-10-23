db3d9ecf5b3e88ab1593922c4dee5addaef623fc
boomNDS <boomsixboom@gmail.com>
2019-10-23 13:54:59 +0700

    :heavy_plus_sign: Eureka Config

--- currency_exchange/server.js
+++ currency_exchange/server.js
@@ -5,7 +5,37 @@ const axios = require('axios')
 const cron = require('node-cron');
 const app = express()
 let currency = {}
-// let currency = fetch_one_hours()
+
+// ------------------ Eureka Config --------------------------------------------
+
+const Eureka = require('eureka-js-client').Eureka;
+
+const eureka = new Eureka({
+  instance: {
+    app: 'currency_exchange',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    statusPageUrl: 'http://localhost:3000',
+    port: {
+      '$': 3000,
+      '@enabled': 'true',
+    },
+    vipAddress: 'localhost',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    }
+  },
+  eureka: {
+    host: 'localhost',
+    port: 8761,
+    servicePath: '/eureka/apps/'
+  }
+});
+eureka.logger.level('debug');
+eureka.start(function(error){
+  console.log(error || 'complete');
+});
 
 //  set schedule fetch_one_hours()
 let task = cron.schedule('*/10 * * * * *', () => {
@@ -30,9 +60,15 @@ app.get('/services/exchange', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log('Start server at port 3000.')
-})
+// ------------------ Server Config --------------------------------------------
+let server = app.listen(3000, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log('Listening at http://%s:%s', host, port);
+});
+// app.listen(3000, () => {
+//   console.log('Start server at port 3000.')
+// })
 
 // fetch data every 1 hour
 async function fetch_one_hours() {
