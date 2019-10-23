01ed30919d6c7431c11141ccd17c0e2037dcf1d8
apichart2425 <packapichart@gmail.com>
2019-10-23 17:25:22 +0700

    add JavaBean id in model

--- transactions/src/main/java/sop/service/transactions/model/Log.java
+++ transactions/src/main/java/sop/service/transactions/model/Log.java
@@ -45,6 +45,14 @@ public class Log {
     @Temporal(TemporalType.TIMESTAMP)
     public Date created_on;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public int getAccount_source() {
         return account_source;
     }
