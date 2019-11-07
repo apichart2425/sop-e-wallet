e223d82fc7cf6f4088fb3e3c1f9a668cc8870c89
Wiput Pootong <wiput.pootong@gmail.com>
2019-11-07 11:05:46 +0700

    :sparkles: add production config to zuul

--- zuul-server/src/main/java/sop/ewallet/zuul/ZuulApplication.java
+++ zuul-server/src/main/java/sop/ewallet/zuul/ZuulApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableZuulProxy
 public class ZuulApplication {
 
