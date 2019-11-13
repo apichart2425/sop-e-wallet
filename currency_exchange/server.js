018e0bb51af4504434509f58827164d89171084f
Wiput Pootong <wiput.pootong@gmail.com>
2019-11-13 15:18:13 +0700

    :sparkles: add docker support

--- currency_exchange/server.js
+++ currency_exchange/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
 const axios = require('axios')
 const cron = require('node-cron');
 const app = express()
@@ -10,7 +9,7 @@ let base = "THB"
 
 //  set schedule fetch_one_hours()
 let task = cron.schedule('0 0 */1 * * *', () => {
-  console.log('\nrunning a task every 1 hours');
+  console.log('\nRunning a task every 1 hour');
   fetch_one_hours(base)
 });
 task.start()
@@ -20,38 +19,42 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-// app.get('/', (req, res) => {
-//   res.send('Currency Exchange Service method GET /services/exchange to get currency exchage (base on THB) or /services/exchange?base=currency currency support [USD, THB, JPY, CNY, EUR, SGD]')
-// })
+function createJsonResponse(status, payload) {
+  return {
+    status,
+    payload
+  }
+}
 
 // get currency exchange base THB
 app.get('/', (req, res) => {
-  let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(ip,"/currency?base=",req.query.base)
-  if (req.query.base === undefined || req.query.base === null || list_currency.indexOf(req.query.base.toUpperCase()) === -1) {
-    res.send(currency['THB'])
+  const base = req.query.base
+  if (base === undefined || base === null) {
+    return res.send(createJsonResponse('success', currency['THB']))
+  } else if (list_currency.indexOf(base.toUpperCase()) === -1) {
+    return res.send(createJsonResponse('error', { message: 'Unsupport currency' }))
   } else {
-    res.send(currency[req.query.base.toUpperCase()])
+    return res.send(createJsonResponse('success', currency[base.toUpperCase()]))
   }
 })
 
 // ------------------ Server Config --------------------------------------------
 let server = app.listen(3000, function () {
-  let host = server.address().address
   let port = server.address().port
-  console.log('Listening at http://%s:%s', host, port)
+  console.log('Listening at port %s', port)
   fetch_one_hours(base)
-  console.log("Start! fetch currency")
+  console.log("Start!")
 });
 
 // fetch data every 1 hour
-async function fetch_one_hours() {
-  // build api URL with user zip
-  console.log("currency has been updated")
-  await list_currency.forEach(base_on => {
+function fetch_one_hours() {
+  const currency_fetch = list_currency.map(async base_on => {
     const baseUrl = 'https://api.exchangeratesapi.io/latest?base=' + base_on
-    axios.get(baseUrl)
-      .then((res) => currency[base_on] = res.data)
+    let response = await axios.get(baseUrl)
+
+    currency[base_on] = response.data.rates
   });
 
+  Promise.all(currency_fetch)
+  console.log("Currency rates have been updated")
 }
