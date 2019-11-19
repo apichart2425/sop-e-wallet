06bd14c77f6962a15ca3081b9cc207541105fc10
Wiput Pootong <wiput.pootong@gmail.com>
2019-11-19 15:19:59 +0700

    :ambulance: optimize import

--- account/src/main/java/sop/ewallet/account/controller/AccountController.java
+++ account/src/main/java/sop/ewallet/account/controller/AccountController.java
@@ -1,8 +1,15 @@
 package sop.ewallet.account.controller;
 
-import org.apache.catalina.User;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import sop.ewallet.account.model.Account;
 import sop.ewallet.account.model.UserRequest;
@@ -11,23 +18,20 @@ import sop.ewallet.account.repositories.AccountRepositories;
 import sop.ewallet.account.repositories.ActionNotMatchingException;
 import sop.ewallet.account.repositories.ResourceNotFoundException;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/")
 public class AccountController {
-//    Repositories
+    @Value("service.transaction")
+    private String transactionUrl;
+
     @Autowired
     private AccountRepositories accountRepositories;
 
     private  RestTemplate restTemplate = new RestTemplate();
-//    public AccountController(RestTemplateBuilder restTemplateBuilder) {
-//        this.restTemplate = restTemplateBuilder.build();
-//    }
 
     private void saveLog(UserRequest ur){
         System.out.println("Save Transaction");
-        String url = "http://localhost:9634/saveLog";
+        String url = transactionUrl + "/saveLog";
         this.restTemplate.postForObject(url, ur,UserRequest.class);
     }
 
@@ -38,25 +42,23 @@ public class AccountController {
         String url;
         switch (ur.getAction()){
             case "deposit":
-                url = "http://localhost:9634/deposit";
+                url = transactionUrl + "/deposit";
                 break;
             case "withdraw":
-                url = "http://localhost:9634/withdraw";
+                url = transactionUrl + "/withdraw";
                 break;
             case "transfer":
                 ur.getAccount_destination().setWallet(accountRepositories.getOne(ur.getAccount_destination().getId()).getWallet());
-                url = "http://localhost:9634/transfer";
+                url = transactionUrl + "/transfer";
                 break;
             default:
-                url = "http://localhost:9634/transaction";
+                url = transactionUrl + "/transaction";
         }
 
-        UserRequest response = this.restTemplate.postForObject(url, ur,UserRequest.class);
-//        System.out.println(response);
-        return response;
+        return this.restTemplate.postForObject(url, ur,UserRequest.class);
     }
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @GetMapping("/")
     public String hello() {
         return "Welcome to You Wallet";
     }
@@ -70,7 +72,7 @@ public class AccountController {
     public Account createDefault(){
         return accountRepositories.save(new Account());
     }
-//
+
     @PostMapping(value = "/deposit")
     public Account deposit(@RequestBody UserRequest ur) throws ActionNotMatchingException {
 //        RequestAction re = new RequestAction("DP", ur.getBalance(), ur.getCurrency_origin(), accountRepositories.getOne((long) ur.getId()));
@@ -85,7 +87,7 @@ public class AccountController {
         }
         return nur.getAccount_source();
     }
-//
+
     @PostMapping(value = "/withdraw")
     public Account withdraw(@RequestBody UserRequest ur) throws ActionNotMatchingException {
         if(!ur.getAction().equals("withdraw")){
@@ -99,7 +101,7 @@ public class AccountController {
         }
         return nur.getAccount_source();
     }
-//
+
     @PostMapping (value = "/transfer")
     public Account[] transfer(@RequestBody UserRequest ur) throws ActionNotMatchingException {
         if(!ur.getAction().equals("transfer")){
@@ -114,12 +116,7 @@ public class AccountController {
         }
         return new Account[]{nur.getAccount_source(), nur.getAccount_destination()};
     }
-//
-////    @GetMapping(value = "/query/{id}")
-////    public double test(@PathVariable int id){
-////        return accountRepositories.cruMoney(id);
-////    }
-//
+
     private void induct(Wallet wd, String currency, int id){
         accountRepositories.findById(id)
                 .map(account -> {
